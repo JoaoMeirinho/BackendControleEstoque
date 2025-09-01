7ed62a779d90975076f20edc46e58e1d02967ede
João Meirinho <meirinhojoao2004@gmail.com>
2025-09-01 20:43:52 -0300

    feat: implement Item model

---
+++ BackendControleEstoque/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendControleEstoque.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime LastModified { get; set; }
+
+    }
+}
