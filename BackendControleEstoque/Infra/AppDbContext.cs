78d5d64dea0ed6535db1ea69a79c60cfb7b3841c
João Meirinho <meirinhojoao2004@gmail.com>
2025-09-01 18:41:57 -0300

    Adicionar arquivos de projeto.

---
+++ BackendControleEstoque/Infra/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendControleEstoque.Infra
+{
+    class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
